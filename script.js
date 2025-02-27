9ed6b446a14ed0cd8a449f70eacb0a11a0c5697d
Mayron Castillo <12mayron12@gmail.com>
2025-02-27 14:19:15 -0600

    Personajes de Rick and Morty

---
+++ script.js
@@ -0,0 +1,40 @@
+const main = document.querySelector(".main");
+const section = document.querySelector(".section");
+const nameMain = document.querySelector(".name");
+const actualStatus = document.querySelector(".status");
+const species = document.querySelector(".species");
+const gender = document.querySelector(".gender");
+const image = document.querySelector(".image");
+
+
+apiUrl = "https://rickandmortyapi.com/api/character"
+async function rickAndMorty(){
+    try {
+        const response = await fetch(apiUrl);
+        if(!response.ok){
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log(data.results);
+        
+        main.textContent = '';
+        data.results.forEach(element => {
+            const characterDiv = document.createElement('div');
+            characterDiv.classList.add("character");
+
+            characterDiv.innerHTML = `
+            <h2 class="name">${element.name}</h2>
+            <p class="status"> <strong>Status:</strong> ${element.status}</p>
+            <p class="species"><strong>Species:</strong> ${element.species}</p>
+            <p class="gender"><strong>Gender:</strong> ${element.gender}</p>
+            <img class="image" src="${element.image}" alt="${element.name}">`;
+
+            main.appendChild(characterDiv);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+
+rickAndMorty();
